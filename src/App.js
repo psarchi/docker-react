071de18519470d8c3dd0ab57c0dac7e91e266965
psarchi <30491257+psarchi@users.noreply.github.com>
2018-11-08 19:26:18 +0400

    testing cloudbuild

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            i Was changed on the feature branch
+            automated deployment
           </p>
           <a
             className="App-link"
